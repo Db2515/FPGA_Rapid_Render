78199051dc6726036a618cf5130c9fee36deff7f
Db2515 <daniel@boulby.plus.com>
2019-04-25 18:41:33 +0100

    Modify script to work for example image size

--- scripts/show_output.py
+++ scripts/show_output.py
@@ -1,14 +1,14 @@
 from PIL import Image, ImageColor
 import sys
 
-im = Image.new("RGBA", (1080, 2160), "black");
+im = Image.new("RGBA", (32, 32), "black");
 
 with open(sys.argv[1]) as file:
 	for line in file:
 		vars = line.strip().split(",");
-		r = int(vars[2][6:8], 16);
+		r = int(vars[2][2:4], 16);
 		g = int(vars[2][4:6], 16);
-		b = int(vars[2][2:4], 16);
+		b = int(vars[2][6:8], 16);
 		a = int(vars[2][0:2], 16);
 		
 		#print(vars[0], vars[1], vars[2], r, g, b, a);
