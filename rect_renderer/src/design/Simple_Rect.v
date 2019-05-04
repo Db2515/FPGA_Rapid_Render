90455b409d3110e5af8af647c919387608566808
Db2515 <daniel@boulby.plus.com>
2019-05-04 20:36:01 +0100

    Change rect renderer to use non blocking assignment for regs

--- rect_renderer/src/design/Simple_Rect.v
+++ rect_renderer/src/design/Simple_Rect.v
@@ -18,11 +18,8 @@ module rect_renderer #(parameter SHAPE_ID = 0)
     
     wire [31:0] color_tmp;
     
-    
-    assign color_tmp = program_in != 0 ? data_in : 
-                                            (x >= xcoord & x < xcoord + width
-                                                & y >= ycoord & y < ycoord + height
-                                                ? color : data_in);
+    wire inshape = x >= xcoord & x < xcoord + width
+                                                & y >= ycoord & y < ycoord + height;
     
     //Inputs
     always @(posedge clk) 
@@ -31,30 +28,30 @@ module rect_renderer #(parameter SHAPE_ID = 0)
             //Change reg with ID = to y
             begin
                 if (y == 0) begin
-                xcoord = data_in;
+                xcoord <= data_in;
                 end
                 else if (y == 1) begin
-                    ycoord = data_in;
+                    ycoord <= data_in;
                 end
                 else if (y == 2) begin
-                    width = data_in;
+                    width <= data_in;
                 end
                 else if (y == 3) begin
-                    height = data_in;
+                    height <= data_in;
                 end
                 else if (y == 4) begin
-                    color = data_in;
+                    color <= data_in;
                 end
             end
     end
     
     always @(posedge clk) begin
-        program_out = program_in;
-        x_out = x;
-        y_out = y;
+        program_out <= program_in;
+        x_out <= x;
+        y_out <= y;
             
         // If program_in  != 0 we are reprogramming a shape so pass inputs through
         
-        data_out = color_tmp;
+        data_out <= !program_in && inshape ? color : data_in;
     end
 endmodule
